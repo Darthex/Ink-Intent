d4d1ab15653d358fb664f4eb87caa497d9d81ee4
Darthex <mrharshulsharma@gmail.com>
2024-10-03 11:40:15 +0530

    added home route

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,11 @@
+import styles from './home.module.css';
+
+const Home = () => {
+	return (
+		<div className={styles.container}>
+			<h1 className={styles.title}>Ink & Intent</h1>
+		</div>
+	);
+};
+
+export default Home;
--- src/root.tsx
+++ src/root.tsx
@@ -1,5 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+
+import Home from './pages/home/home.tsx';
+
+import { ROUTES } from './constants/routes.ts';
+
 function Root() {
-	return <div>Project Setup</div>;
+	return (
+		<Routes>
+			<Route path={ROUTES.HOME} element={<Home />} />
+		</Routes>
+	);
 }
 
 export default Root;
